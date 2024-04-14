df4b99774c0cbebd2b4ecbd40a1a4e7125821907
NakiAyame <annguuenhuu9@gmail.com>
2024-04-15 00:21:37 +0700

    up code javascript 1

---
+++ Esucational/Escational.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('show-header').addEventListener('click', function(){
+        document.querySelector('right-menu').classList.add('active')
+    });
+
+    document.getElementById('close').addEventListener('click', function(){
+        document.querySelector('right-menu').classList.add('active')
+    });
+});
+
