d4ca6472daa4a715d61b2d724f05bd943f79925c
NakiAyame <annguuenhuu9@gmail.com>
2024-04-15 02:00:47 +0700

    update final web educational

--- Esucational/Escational.js
+++ Esucational/Escational.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function(){
     document.getElementById('show-header').addEventListener('click', function(){
-        document.querySelector('right-menu').classList.add('active')
+        document.querySelector('.right-menu').classList.add('active')
     });
 
     document.getElementById('close').addEventListener('click', function(){
-        document.querySelector('right-menu').classList.add('active')
+        document.querySelector('.right-menu').classList.remove('active')
     });
 });
 
